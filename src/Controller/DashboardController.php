51e210180382eea1b122be852c808b142cb4cc20
dhia-djobbi <dhia.djobbi@avaxia-group.com>
2021-11-07 19:14:52 +0100

    dashboard

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/dashboard", name="dashboard")
+     * @IsGranted("ROLE_USER")
+     */
+    public function index(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
