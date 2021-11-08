b98e57d451d04cef8953ff0cf15d187e608af2bd
dhia-djobbi <dhia.djobbi@avaxia-group.com>
2021-11-08 11:34:43 +0100

    Article CRUD

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/article")
+ * @IsGranted("ROLE_USER")
+ */
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", name="article_index", methods={"GET"})
+     */
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="article_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article, [
+            'validation_groups' => ["Default",'create'],
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setAuthor($this->getUser());
+
+            //image uploading
+            $image = $form->get('image')->getData();
+            $fileName = md5(uniqid()) . '.' . $image->guessExtension();
+            $image->move($this->getParameter('articles_directory'), $fileName);
+            $article->setImage($fileName);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('article/new.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="article_show", methods={"GET"})
+     */
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="article_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Article $article): Response
+    {
+        $originalImage = $article->getImage();
+        $form = $this->createForm(ArticleType::class, $article, [
+            'validation_groups' => ["Default"],
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $image = $form->get('image')->getData();
+            if ($image) {
+                //nfassa5 fel old image
+                $fs = new Filesystem();
+                $fs->remove($this->getParameter('articles_directory') . "/" . $originalImage);
+                
+                $fileName = md5(uniqid()) . '.' . $image->guessExtension();
+                $image->move($this->getParameter('articles_directory'), $fileName);
+                $article->setImage($fileName);
+            } 
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('article_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('article/edit.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="article_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Article $article): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('article_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
@@ -19,16 +20,27 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="The title is required" )
+     * @Assert\Length(min="6", minMessage="The title must be at least {{ limit }} characters long")
+     * @Assert\Type(type="string", message="The title must be a string")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="The content is required")
+     * @Assert\Length(min="10", minMessage="The content must be at least {{ limit }} characters long")
+     * @Assert\Type(type="string", message="The content must be a string")
      */
     private $content;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank(message="The image is required" , groups={"create"})
+     * @Assert\File(
+     *      mimeTypes = {"image/png", "image/jpeg"},
+     *      mimeTypesMessage = "Please upload a valid Image (PNG  or JPEG)"
+     *  )
      */
     private $image;
 
@@ -43,6 +55,12 @@ class Article
      */
     private $createdAt;
 
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -53,7 +71,7 @@ class Article
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    public function setTitle(?string $title): self
     {
         $this->title = $title;
 
@@ -65,19 +83,19 @@ class Article
         return $this->content;
     }
 
-    public function setContent(string $content): self
+    public function setContent(?string $content): self
     {
         $this->content = $content;
 
         return $this;
     }
 
-    public function getImage(): ?string
+    public function getImage()
     {
         return $this->image;
     }
 
-    public function setImage(?string $image): self
+    public function setImage($image)
     {
         $this->image = $image;
 
